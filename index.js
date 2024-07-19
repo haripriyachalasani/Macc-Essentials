b9ac079c26e7607e6d8f9cff865164e410a9f5c7
Hari Priya <priyachalasani1@gmail.com>
2024-07-19 22:45:49 +0530

    add these files to the repo

---
+++ index.js
@@ -0,0 +1,10 @@
+// nav code start
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+// Nav code end
